ad8c561fcc8081bf0525d27de0c05c8caaaebbbd
Martha <mm.blanco1020@gmail.com>
2018-10-19 14:13:52 -0500

    First commit

---
+++ Git Assignment/src/ListElement.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+
+/**
+ * @author ADMIN
+ *
+ */
+public class ListElement 
+{
+	private ListElement next;
+	private ListElement previous;
+	private int data;
+
+	public ListElement()
+	{
+		this.data=0;
+		this.next=null;
+		this.previous=null;
+	}
+	
+	/**
+	 * @param data
+	 *   an integer to be stored in the node
+	 *@return none
+	 */
+	public void setData(int data)
+	{
+		this.data=data;
+	}
+	
+	public int getData()
+	{
+		return this.data;
+	}
+
+}
