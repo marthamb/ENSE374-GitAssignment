bd635082657e531db1d37644ddd974724d915721
Martha <mm.blanco1020@gmail.com>
2018-10-25 17:39:19 -0500

    Definition of LinkedList class and what it should do

---
+++ Git Assignment/src/LinkedList.java
@@ -0,0 +1,64 @@
+
+public class LinkedList
+{
+	 /**
+     * Head
+     */
+	private ListElement leHead;
+	
+	 /**
+     * Tail
+     */
+	private ListElement leTail;
+	
+	 /**
+     * pointer
+     */
+	private ListElement le;
+	
+	/**
+     * Number of element
+     */
+	int elementCount;
+
+	
+	public static void main()//String[] args)
+	{
+		LinkedList SingleLL=new LinkedList();
+		ListElement leNew= new ListElement();
+		SingleLL.addElement(leNew);
+	}
+	
+	//Add a node
+	public void addElement(ListElement le)
+	{
+		//is this the first element
+		
+	}
+	
+	//Retrieve a node 
+	public ListElement getElement(int index)
+	{
+		
+	}
+
+	// Delete a node 
+	public ListElement deleteElement(int index)
+	{
+		
+	}
+	
+	// Print the linked list from the tail
+	public void printLinkedListTail()
+	{
+		
+	}
+	
+	// Print the linked list from the head
+	 public void printLinkedListHead()
+	 {
+		 
+	 }
+
+
+}
